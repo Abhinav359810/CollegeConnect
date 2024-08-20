d57ad8a877d06eb79663184a1d3a440475672442
Abhinav359810 <abhinavsinghairclay@gmail.com>
2024-08-20 18:47:45 +0530

    updatefrontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,22 @@ import './CSS/Header.css';
 
 function Header() {
   return (
-    <nav className="header">
+    <header className="header">
       <div className="logo">CollegeConnect</div>
+      
+      {/* Move the nav-links to the left */}
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-    </nav>
+      
+      {/* Auth buttons on the right */}
+      <div className="auth-buttons">
+        <button className="btn">Login</button>
+        <button className="btn">Sign Up</button>
+      </div>
+    </header>
   );
 }
 
